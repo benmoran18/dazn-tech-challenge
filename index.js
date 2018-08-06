9fbdecd8698a875b513aec619139d36f16e98bbd
Ben Moran <ben.moran@kwiff.com>
2018-08-06 21:10:50 +0100

    Created new server class from initial code in index.js file Refactored main index.js file to use new server class Separated routes into individual file Separated config into external json file

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const express = require('express')
-const app = express()
-
-app.get('/connection', (req, res, next) => {
-  res.json({success: false, message:'not yet implemented'})
-})
-
-app.listen(9000, () => console.log('Listening...'))
+const RestServer = require('./lib/server')
+const server = new RestServer(require('./config'))
+server.start()
