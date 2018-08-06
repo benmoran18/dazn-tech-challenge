16cc19caf3ed03a1a0489aa22c9fd7d80cae019e
Ben Moran <ben.moran@kwiff.com>
2018-08-06 22:46:36 +0100

    Created connection pool module for managing connections for users, Created connection module for instantiating connections for users, Moved connection routes to connection-pool.js file for consistency, Updated server listening message to display port number, Updated model tests for connection pool, Updated README file with response examples for main endpoint

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ To run locally just use:
 ```
 nodemon .
 ```
+
+### Endpoints
+**GET /connections/new/:userId**
+
+Creates a new connection for the specified user id.
+
+*Example Success Response*
+```
+{
+    "success": true,
+    "connection": {
+        "connectionId": "123-2018-08-06T21:12:43.182Z",
+        "userId": "123"
+    }
+}
+```
+
+*Example Failed Response*
+```
+{
+    "success": false,
+    "message": "no connections available for user 123"
+}
+```
---
+++ test/models/connection-pool.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai')
+const config = require('../../config')
+const connectionPool = require('../../lib/models/connection-pool')
+const userId = 123
+
+describe('Connection pool', async () => {
+  for(let i = 0; i < config.maximumUserConnections; i++) {
+    it(`should create connection ${i+1}`, async () => {
+      const connection = await connectionPool.getConnection(userId)
+      expect(connection).to.have.property('userId')
+      expect(connection).to.have.property('connectionId')
+    })
+    it(`should update the connection count to ${i + 1}`, async () => {
+      expect(await connectionPool.getConnectionCount(userId)).to.equal(i+1)
+    })
+  }
+  it('should fail getting connection', async () => {
+    try {
+      const connection = await connectionPool.getConnection(userId)
+      return false
+    } catch (error) {
+      expect(error).to.be.equal('Maximum number of connections exceeded')
+    }
+  })
+})
